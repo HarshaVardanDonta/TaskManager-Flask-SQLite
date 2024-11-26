d9f160856c1ee26cfc7dc812045ec5b9c91f029a
Harsha <dharsha227@gmail.com>
2024-11-26 14:06:39 +0530

    port update

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def delete_task(task_id):
     return jsonify({'message': f'Task with id {task_id} deleted'})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8055)
