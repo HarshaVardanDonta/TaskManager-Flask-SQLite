09e243ff2cf9c997879df56e7616b08c0621bf5f
Harshavardhan Donta <94307837+HarshaVardanDonta@users.noreply.github.com>
2024-12-08 23:12:09 +0530

    Update application.py

--- application.py
+++ application.py
@@ -24,7 +24,6 @@ def query_db(query, args=(), one=False):
     cur = conn.cursor()
     cur.execute(query, args)
     rv = cur.fetchall()
-    conn.commit()
     conn.close()
     return (rv[0] if rv else None) if one else rv
 
